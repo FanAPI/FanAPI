cef8c82deb843b33261c7424a861c234b74fb95a
FanAPI <148718580+FanAPI@users.noreply.github.com>
2023-10-22 22:25:45 +0300

    Add files via upload

---
+++ fanapi.py
@@ -0,0 +1,24 @@
+import requests
+API_BASE_URL = 'https://41c6-77-50-109-213.ngrok-free.app/fanapi'
+def generate_api_key(api_key, user_id):
+    response = requests.post(f'{API_BASE_URL}/generate', headers={'Authorization': api_key}, params={'user_id': user_id})
+    return response.json()
+
+def get_api_info(api_key, marker):
+    response = requests.get(f'{API_BASE_URL}/get_info', params={'api_key': api_key, 'marker': marker})
+    return response.json()
+
+def set_balance(api_key, wallet_id, balance):
+    data = {'api_key': api_key, 'wallet_id': wallet_id, 'balance': balance}
+    response = requests.put(f'{API_BASE_URL}/set_balance', json=data)
+    return response.json()
+
+def build_transaction(api_key, wallet_id, transaction_amount, password):
+    data = {'api_key': api_key, 'wallet_id': wallet_id, 'transaction_amount': transaction_amount, 'password': password}
+    response = requests.post(f'{API_BASE_URL}/build_transaction', json=data)
+    return response.json()
+
+def receive_transaction(api_key, check_id, password):
+    data = {'api_key': api_key, 'check_id': check_id, 'password': password}
+    response = requests.post(f'{API_BASE_URL}/receive_transaction', json=data)
+    return response.json()
